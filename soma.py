4922be557fb56404b2889491608a73edd2fb12f6
Iago <iagomontanhini@gmail.com>
2026-01-19 22:01:19 -0300

    adiciona exemplo de soma

---
+++ soma.py
@@ -0,0 +1,5 @@
+#nesse código será implementada a função soma que recebe dois números e retorna a soma deles. Para isso iremos utilizar o imput e a soma.
+num1=int(input("Digite o primeiro número: "))
+num2=int(input("Digite o segundo número: "))
+soma= num1 + num2
+print(f"A soma dos dois números {num1} e {num2} é:", soma)
---
+++ teste miguel.py
@@ -0,0 +1,6 @@
+n1= input("Qual seu nome?  ")
+n2= int(input("quantos anos você tem? "))
+if n2 <= 10:
+    print("novo")
+else:
+    print("véio")
